c4d59b731e920e892a8017f9b89057260e53bf1f
Xmaw <elias@Eliass-MBP.lan>
2025-03-02 19:11:00 +0100

    Updated expenses names and added some logic to calculate total expenses.

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,15 @@ public class Main {
             transactionList.add(scanner.nextLine());
         }
         List<String> foodStores = List.of("ica", "willys", "tempo");
-        List<String> pleasureNames = List.of("gamer2gamer", "frisorateljen");
-        List<String> recurringNames = List.of("vimarhem", "omsättning lån", "vimmerby ene", "telia", "alfa kassan", "hallon", "raidbots premium", "1password", "blizzard entertain", "youtube", "max", "hyresgästför", "spotify");
-        List<String> restaurantNames = List.of("foodora", "sibylla", "big wong", "monte car", "burger king");
-        List<String> ignoredNames = List.of("överföring", "autogiro", "klarna refun");
+        List<String> pleasureNames = List.of("gamer2gamer", "frisorateljen", "intermezzo", "blizzard eu", "barbershop", "klarna", "klipphornet", "handelsboden");
+        List<String> recurringNames = List.of("vimarhem", "omsättning lån", "vimmerby ene", "telia", "alfa kassan", "hallon", "raidbots premium", "1password", "blizzard entertain", "youtube", "max", "hyresgästför", "spotify", "akka");
+        List<String> restaurantNames = List.of("foodora", "sibylla", "big wong", "monte car", "burger king", "brygghuset", "palma", "bakfickan");
+        List<String> ignoredNames = List.of("överföring", "klarna refun");
         List<String> incomeSourceNames = List.of("lön");
         List<String> clothesStoreNames = List.of("dressman", "intersport");
         List<String> alcoholAndBarNames = List.of("best western", "kharma", "pink bistro", "systembolaget");
+        List<String> repaymentLoanNames = List.of("resurs ba", "csn centrala", "centrala stu");
+        List<String> vehicleExpenseNames = List.of("st1", "circle k", "mekonomen", "tanka", "länsförsäk", "a e bil & tra", "svedea");
 
 
         String[] headers = transactionList.removeFirst().split(";");
@@ -32,17 +34,34 @@ public class Main {
         Category clothes = getCategorySpecificTransaction(transactionList, clothesStoreNames);
         Category alcoholAndBars = getCategorySpecificTransaction(transactionList, alcoholAndBarNames);
         Category income = getCategorySpecificTransaction(transactionList, incomeSourceNames);
+        Category repaymentLoans = getCategorySpecificTransaction(transactionList, repaymentLoanNames);
+        Category vehicle = getCategorySpecificTransaction(transactionList, vehicleExpenseNames);
+
+        List<Category> allExpenses = List.of(food, recurring, restaurants, pleasure, clothes, alcoholAndBars, repaymentLoans, vehicle);
+        double totalExpenses = calculateTotal(allExpenses);
 
         printRemainingTransactions(transactionList);
         System.out.println("----------------------------------------");
-        System.out.println("Ignored: " + ignored);
+        //System.out.println("Ignored: " + ignored);
         System.out.println("Groceries: " + food.getTotal());
         System.out.println("Recurring: " + recurring.getTotal());
         System.out.println("Restaurants:" + restaurants.getTotal());
         System.out.println("Pleasure: " + pleasure.getTotal());
         System.out.println("Clothes: " + clothes.getTotal());
         System.out.println("Bars: " + alcoholAndBars.getTotal());
+        System.out.println("Repayment loans: " + repaymentLoans.getTotal());
+        System.out.println("Vehicle: " + vehicle.getTotal());
         System.out.println("Income: " + income.getTotal());
+        System.out.println("Total expenses: " + totalExpenses);
+        System.out.println("Remaining: " + (income.getTotal() + totalExpenses));
+    }
+
+    private static double calculateTotal(List<Category> transactions) {
+        double total = 0d;
+        for (Category category : transactions) {
+            total += category.getTotal();
+        }
+        return total;
     }
 
     private static void printRemainingTransactions(ArrayList<String> transactionList) {
